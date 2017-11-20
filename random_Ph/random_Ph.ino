e3751f3f92aebea38a5295e27684195a558f05b7
samuil1998 <32335781+samuil1998@users.noreply.github.com>
2017-11-20 17:46:47 +0000

    Arduino code for testing random pH
    returns a random value between 0 and 14 every second

---
+++ random_Ph/random_Ph.ino
@@ -0,0 +1,16 @@
+int pH = 0;
+
+void setup()
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+  pH = random(15);
+  Serial.println(pH);
+  delay(1000);
+  
+}
